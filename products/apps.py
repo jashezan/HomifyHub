89837cbd0a1f107b41738e875ffc99214b7c31d1
Jubyer Ahmed Shezan <84038649+jashezan@users.noreply.github.com>
2025-02-05 20:28:09 +0600

    fix(Design): The design on forms was not working, fixed it with editing forms.py

--- products/apps.py
+++ products/apps.py
@@ -1,6 +1,10 @@
+"""This file is used to configure the app name for the products app."""
 from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
+    """
+    Configuration for products app
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
--- products/forms.py
+++ products/forms.py
@@ -1,16 +1,35 @@
 """
-This file is used to create a form for the Product model.
+Forms for products app
 """
+
 from django import forms
 from .models import Product
 
+
 class ProductForm(forms.ModelForm):
     """
-    Product form
+    Product form with Bootstrap styling
     """
+
     class Meta:
         """
-        Meta class
+        Meta class for ProductForm
         """
+
         model = Product
-        fields = ['name', 'description', 'price']
+        fields = ["name", "description", "price"]
+        widgets = {
+            "name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Enter product name"}
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 20,
+                    "placeholder": "Enter product description",
+                }
+            ),
+            "price": forms.NumberInput(
+                attrs={"class": "form-control", "placeholder": "Enter price"}
+            ),
+        }
